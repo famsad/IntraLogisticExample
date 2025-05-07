dbb224ea483427053682b7c75df51f70c75a4c0b
famsad <47644643+famsad@users.noreply.github.com>
2025-05-07 08:11:58 +0200

    Update Order.cs
    removed unused seter and updated getter

--- Warehouse/Logistics/Order.cs
+++ Warehouse/Logistics/Order.cs
@@ -30,7 +30,7 @@ public class Order : BaseEntity, IUsesTimeStamps
 
     //also mockup as no event triggering is implemented at the moment.
     //But as it is used in the priority determination it is implemented as this mockup.
-    public DateTime DateCreated { get => DateTime.UtcNow; set => DateCreated = DateTime.UtcNow; }
-    public DateTime DateModified { get => DateTime.UtcNow; set => DateModified = DateTime.UtcNow; }
+    public DateTime DateCreated { get; }
+    public DateTime DateModified { get; }
 }
 
